6071744096a7539ac6d825971068635976e3ac64
Birla <birlatamil860@gmail.com>
2025-04-12 13:15:10 +0530

    did nothing

--- app.py
+++ app.py
@@ -72,6 +72,7 @@ def view_reports():
         reports = c.fetchall()
     return render_template('view_reports.html', reports=reports)
 
+print(1    )
 if __name__ == '__main__':
     init_db()
     app.run(debug=True)
